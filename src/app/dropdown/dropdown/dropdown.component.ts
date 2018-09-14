a92f256aa0fc9a7312758bf137f777c3e6d97576
Murad Khan <mkhan@sbmcorp.com>
2018-09-14 10:18:45 -0700

    Added keyboard functionality to single select dropdown

--- src/app/dropdown/dropdown/dropdown.component.ts
+++ src/app/dropdown/dropdown/dropdown.component.ts
@@ -1,4 +1,11 @@
-import { Component, Input, OnInit, Output, EventEmitter } from '@angular/core';
+import {
+  Component,
+  Input,
+  Output,
+  EventEmitter,
+  ViewChild,
+  ElementRef
+} from '@angular/core';
 
 export interface DropdownItem {
   Name: string;
@@ -10,32 +17,62 @@ export interface DropdownItem {
   templateUrl: './dropdown.component.html',
   styleUrls: ['./dropdown.component.scss', '../shared/dropdown.scss']
 })
-export class DropdownComponent implements OnInit {
+export class DropdownComponent {
   @Input() placeholder: string;
   @Input() items: Array<DropdownItem>;
 
   @Output() selected = new EventEmitter<DropdownItem>();
+  @ViewChild('button') button: ElementRef;
 
+  
   state = {
-    open: false
+    open: false,
+    hovered: -1
   }
-  constructor() { }
-
-  ngOnInit() {
-  }
-
+  
   toggleDropdown(event) {
     this.state.open = !this.state.open;
-    event.stopPropagation()
+    this.state.hovered = -1;
+    event.stopPropagation();
     if (this.state.open) {
-          // create an event listener to close dropdown
-          let eventFn = (e) => {
-            this.state.open = false;
-            window.removeEventListener('click', eventFn);
-          };
-          // when a user clicks anywhere on the page
-          window.addEventListener('click', eventFn);
+      // In request animation frame so dropdown element exists before setting listeners
+      window.requestAnimationFrame( () => {
+        this.setUpListeners();
+        this.button.nativeElement.focus()
+      });
     }
+  }
+  setUpListeners() {
+    let keyboardListenerFn = (e) => {
+      switch (e.keyCode) {
+        // Up Arrow
+        case (38):
+          this.moveHoveredState(-1);
+          break;
+        // Down Arrow
+        case (40):
+          this.moveHoveredState(1);
+          break;
+        // Enter Key
+        case (13):
+          this.output(this.items[this.state.hovered]);
+      }
+    }
+    let closeListenerFn = (e) => {
+      this.state.open = false;
+      window.removeEventListener('click', closeListenerFn);
+      this.button.nativeElement.removeEventListener('keydown', keyboardListenerFn);
+    };
+
+    this.button.nativeElement.addEventListener('keydown', keyboardListenerFn);
+    window.addEventListener('click', closeListenerFn);
+  }
+  moveHoveredState(direction: 1 | -1) {
+    let nextState = this.state.hovered + direction;
+    if (nextState < 0) nextState = 0;
+    if (nextState > this.items.length - 1) nextState = this.items.length - 1;
+
+    this.state.hovered = nextState;
   }
   output(item: DropdownItem) {
     this.selected.emit(item);
