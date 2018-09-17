93f5d4104e23f057b063c7f8ca9f8c1c8dea2edb
Murad Khan <mkhan@sbmcorp.com>
2018-09-17 09:24:11 -0700

    Testing concurrency of observables

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
-import { Component, ChangeDetectorRef } from '@angular/core';
+import { Component, ChangeDetectorRef, OnInit } from '@angular/core';
 import { mock } from './flex-graph/mock';
+import { Observable } from 'rxjs/Observable';
+import 'rxjs/add/observable/of';
+import { delay } from 'rxjs/operators/delay'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   constructor(
     private cdr: ChangeDetectorRef
   ) {}
@@ -20,6 +24,12 @@ export class AppComponent {
       row1: false,
       row2: false
     }
+  }
+
+  ngOnInit() {
+    Observable.of(5).pipe(delay(500)).subscribe( () => console.log('OBS 1'))
+    Observable.of(5).pipe(delay(250)).subscribe(() => console.log('OBS 2'))
+    Observable.of(5).pipe(delay(100)).subscribe(() => console.log('OBS 3'))
   }
 
   selected(item) {
